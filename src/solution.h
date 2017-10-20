09669515c86802ad40901650bab6281ec2d4995a
Yoctoboy <alexis.reymann@telecom-paristech.fr>
2017-10-20 11:47:20 +0200

    some more attributes are private

--- src/solution.h
+++ src/solution.h
@@ -7,23 +7,22 @@ using namespace std;
 
 class solution {
 public:
-	int size;
-	int Rcap;
-	int Rcom;
-	int nbCapteurs;
 	vector<vector<int> > cover;
-
 	solution(int, int, int);
 	solution() {};
 	vector< pair<int, int> > neighbour_transf(int);
 	solution getNeighbour(pair<int,int>);
 	bool allCover();
 	bool allCommunicate();
-	void addCaptor(pair<int,int>);
-	void removeCaptor(pair<int,int>);
+	bool addCaptor(pair<int,int>);
+	bool removeCaptor(pair<int,int>);
 	void updateCover();
 
 private:
+	int size;
+	int Rcap;
+	int Rcom;
+	int nbCapteurs;
 	vector<vector<bool> > grid;
 };
 
