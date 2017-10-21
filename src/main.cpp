1965045ee3f7b420c9a13f99d688989f024914b4
Yoctoboy <alexis.reymann@telecom-paristech.fr>
2017-10-21 14:18:12 +0200

    Gets initial solution

--- src/main.cpp
+++ src/main.cpp
@@ -13,33 +13,22 @@
 #include <ctime>
 #include "solution.h"
 #include "mhAlea.h"
+#include "init.h"
 
 int main(){
+	freopen("log.txt", "w", stderr);
 	srand(time(NULL));
-	int sizz = 15;
-	solution sol = solution(sizz, 1, 1);
-	/*for (int i = 0; i < sizz; i++) {
-		for (int j = 0; j < sizz; j++) {
-			sol.grid[i][j] = false;
-		}
-	}
-	sol.grid[1][0] = true;
-	sol.grid[1][1] = true;
-	sol.grid[2][1] = true;
-	sol.grid[1][2] = true;
-	sol.grid[3][1] = true;
-	sol.grid[1][3] = true;*/
+	int size = 10, Rcap = 1, Rcom = 1;
+	solution s = get_initial_solution(size, Rcap, Rcom);
+	s.printgrid();
 
-	//cout << sol.pathfinding() << endl;
 
-	puts(sol.allCover() ? "OK" : "NO");
-	puts(sol.allCommunicate() ? "OK" : "NO");
-	mhAlea MH = mhAlea(sizz, 1, 1, 1000);
+	/*mhAlea MH = mhAlea(sizz, 1, 1, 1000);
 	MH.launchMH();
 	mhAlea MH2 = mhAlea(sizz, 1, 1, 1000);
 	MH2.launchMHremove();
 	mhAlea MH3 = mhAlea(sizz, 1, 1, 1000);
 	MH3.launchMHconc();
-	system("pause");
+	system("pause");*/
 	return 0;
 }
