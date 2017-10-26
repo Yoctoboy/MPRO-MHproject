6dfb9f6f4938981116b95edc919c86d71af02312
MadelineFleury <madeline.fleury@eleves.enpc.fr>
2017-10-26 10:21:39 +0200

    whatev

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@
 #include "mhGenetic.h"
 
 int main(){
-	/*freopen("log.txt", "w", stderr);
+	freopen("log.txt", "w", stderr);
 	srand(time(NULL));
 	int size = 10, Rcap = 1, Rcom = 1;
 	puts("initializing m");
@@ -26,20 +26,10 @@ int main(){
 	puts("m initialized");
 	//m.launchMH(1);
 
-	/*s.printgrid(false);
-	printf("Amount of captors : %d/%d\n", s.getCapt(), size*size);
+	//s.printgrid(false);
+	//printf("Amount of captors : %d/%d\n", s.getCapt(), size*size);
 
 
-	mhAlea MH = mhAlea(sizz, 1, 1, 1000);
-	MH.launchMH();
-	mhAlea MH2 = mhAlea(sizz, 1, 1, 1000);
-	MH2.launchMHremove();
-	mhAlea MH3 = mhAlea(sizz, 1, 1, 1000);
-	MH3.launchMHconc();
-	system("pause");*/
-
-	char x = 8;
-	cout << x << endl;
 	system("pause");
 	return 0;
 }
--- src/mhGenetic.cpp
+++ src/mhGenetic.cpp
@@ -24,13 +24,16 @@ mhGenetic::mhGenetic(int poolsize, int rcap, int rcom, int size){
   size = size;
   mask.reserve(100);
   pool = {get_initial_solution(size, Rcap, Rcom, true)};
+  pool.reserve(poolsize);
   generatePool(10);
 }
 
 void mhGenetic::generatePool(int initsize){
+	vector<solution*> store;
+	store.resize(10);
   for(int i = 0; i < initsize; i++){
     printf("%d\n", i);
-    pool.resize(i);
-    pool[i] = (get_initial_solution(size, Rcap, Rcom));
+	solution* s = new solution(size, Rcap, Rcom);
+    store.push_back(s);
   }
 }
