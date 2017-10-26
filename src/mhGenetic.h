8e566207fd44d8a46f7def1174f3ee509c79fdb2
Yoctoboy <alexis.reymann@telecom-paristech.fr>
2017-10-26 09:53:20 +0200

    mhGenetic

---
+++ src/mhGenetic.h
@@ -0,0 +1,24 @@
+#ifndef MHGENETIC_H
+#define MHGENETIC_H
+
+#include <vector>
+#include "solution.h"
+
+class mhGenetic {
+public:
+  vector< solution > pool;
+  vector<int> mask;
+
+  mhGenetic(int,int,int,int);
+  void generateMask();
+  void generatePool(int);
+  void mutate(solution, int);
+
+private:
+  int Rcap;
+  int Rcom;
+  int size;
+  int poolsize;
+};
+
+#endif
