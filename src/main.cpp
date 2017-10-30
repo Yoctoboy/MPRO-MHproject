9f0111664657fd39bf70c85fb0bdeb0fcfa2d725
Alexis R <alexis.reymann@telecom-paristech.fr>
2017-10-30 21:52:40 +0100

    Gives heuristical solution

--- src/main.cpp
+++ src/main.cpp
@@ -20,47 +20,14 @@
 int main(){
 	freopen("log.txt", "w", stderr);
 	srand(time(NULL));
-	int size = 20, Rcap = 1, Rcom = 1;
-	puts("initializing m");
+	int size = 10, Rcap = 1, Rcom = 1;
 	mhGenetic m = mhGenetic(10, Rcap, Rcom, size);
 	m.generateBinaryMask();
-	puts("m initialized");
-	/*pair<solution, solution> t = m.breed(m.pool[0], m.pool[1]);
-	puts("MOTHER");
+	for(int iter = 0; iter < 100 ; iter++){
+		printf("\nIteration %d - BEST = %d\n", iter, m.pool[0].getCapt());
+		m.updatePool(10);
+	}
+	puts("BEST SOLUTION :");
 	m.pool[0].printgrid();
-	puts("FATHER");
-	m.pool[1].printgrid();
-
-	puts("DAUGTHER");
-	t.first.printgrid();
-	puts("DAUGTHER");
-	t.second.printgrid();
-	printf("with %d captors\n", t.first.getCapt());*/
-	for (int t = 0; t < 20000; t++) {
-		m.updatePool(15);
-		if (t % 2000 == t / 2000) {
-			cout << t/2000 << " ";
-			int count = 1000;
-			for (int t = 0; t < m.getPoolsize(); t++) {
-				if (count > m.pool[t].value) {
-					count = m.pool[t].value;
-				}
-			}
-			cout << count << endl;
-			m.pool[0].printgrid();
-		}
-	}
-	int count = 0;
-	for (int t = 0; t < m.getPoolsize(); t++) {
-		count += m.pool[t].value;
-	}
-	cout << "mean captors " << count / m.getPoolsize() << endl;
-	//m.launchMH(1);
-
-	//s.printgrid(false);
-	//printf("Amount of captors : %d/%d\n", s.getCapt(), size*size);
-
-
-	system("pause");
 	return 0;
 }
