872af4843ab7b016ee04b1c0bd712fca1f161a19
Alexis <alexis.reymann@telecom-paristech.fr>
2017-11-04 17:54:59 +0100

    Added writing in file - Preparing for big execution

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,8 @@
 #include <pthread.h>
 #include <thread>
 #include <unistd.h>
+#include <fstream>
+#include <string>
 #include "solution.h"
 #include "mhAlea.h"
 #include "init.h"
@@ -23,10 +25,17 @@
 
 float totalStrength;
 vector<bool> threadsStatus; //true if over, else false
+ofstream outfile;
 
 bool allThreadsOver(){
 	for(int i = 0; i < (int)threadsStatus.size(); i++) if(!threadsStatus[i]) return false;
 	return true;
+}
+
+void writefile(int s, int cap, int com, vector<int> vdisp){
+	outfile << "Size = " << s << " ; Rcap = " << cap << " ; Rcom = " << com << endl;
+	for(int i = 0; i < vdisp.size(); i++) outfile << vdisp[i] << " ";
+	outfile << endl << endl;
 }
 
 //parameter class for every thread
@@ -47,7 +56,8 @@ void* check_finished(void *checkarg){
 }
 
 //executed method for every thread (one instance = one thread)
-void* compute_stuff(void *threadarg){
+void* compute_instance(void *threadarg){
+	vector<int> vdisp;
 	struct threaddata *arg;
 	arg = (struct threaddata *) threadarg;
 	int id = arg->id;
@@ -58,6 +68,7 @@ void* compute_stuff(void *threadarg){
 	mhGenetic m = mhGenetic(15, cap, com, s);
 	m.generateBinaryMask();
 	int curBest = 2000, bestsince = 0;
+	if(s == 30 && cap == 1 && com == 1) vdisp.push_back(m.pool[0].getCapt());
 	for(int iter = 0; iter < maxiter ; iter++){
 		printf("Size %dx%d - Rcap = %d - Rcom = %d // Iteration %d/%d - BEST = %d\n", s, s, cap, com, iter+1, maxiter, m.pool[0].getCapt());
 		if(iter%10 == 0) m.updatePool(20, 0.8, iter, iter-bestsince >= 5 ? 5 : 0);
@@ -67,18 +78,23 @@ void* compute_stuff(void *threadarg){
 			curBest = m.pool[0].getCapt();
 			bestsince = iter;
 		}
+		vdisp.push_back(curBest);
 	}
 	printf("SOLUTION FOUND - Size %dx%d - Rcap = %d - Rcom = %d - CAPT = %d\n", s, s, cap, com, m.pool[0].getCapt());
 	fprintf(stderr, "Size %dx%d - Rcap = %d - Rcom = %d\n", s, s, cap, com);
 	float strength = ((float)m.pool[0].getCapt()*(pow(cap*com, 0.7)) /(s*s));
 	totalStrength += strength;
 	fprintf(stderr, "Strength of solution : %lf\n",  strength);
+
+	writefile(s, cap, com, vdisp);
+
 	m.pool[0].printgrid(true);
 	threadsStatus[id] = true; //thread is over
 	pthread_exit(NULL);
 }
 
 int main(){
+	outfile.open("Evolution.txt");
 	freopen("log.txt", "w", stderr);
 	vector<int> sizes = {10, 15, 20, 25, 30, 40, 50};
 	vector<int> cap = {1, 1, 2, 2, 3, 3};
@@ -97,8 +113,8 @@ int main(){
 			td[instance].cap = cap[ca];
 			td[instance].com = com[ca];
 			td[instance].size = sizes[s];
-			td[instance].maxiter = 50;
-			int rc = pthread_create(&threads[instance], NULL, compute_stuff, (void *)&td[instance]);
+			td[instance].maxiter = 3;
+			int rc = pthread_create(&threads[instance], NULL, compute_instance, (void *)&td[instance]);
 			if(rc){ //thread could not be created
 				printf("Couldn't create thread %d, exiting...\n", instance);
 				exit(-1);
